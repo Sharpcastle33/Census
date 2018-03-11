1f9ba79bf19e5ed4e00d8b0f594ec7b2169107b3
JG <33494600+JJ-GG@users.noreply.github.com>
2018-03-11 12:06:34 -0400

    Made minutes variable for easy interval change

--- src/main/java/com/gmail/sharpcastle33/util/CensusInterval.java
+++ src/main/java/com/gmail/sharpcastle33/util/CensusInterval.java
@@ -6,7 +6,9 @@ import com.gmail.sharpcastle33.Census;
 
 public class CensusInterval extends BukkitRunnable {
 	
-	int time = 20 * 60 * 5;
+	final int minutes = 10; // ADJUST THIS TO ADJUST INTERVAL TIME
+	
+	int time = 20 * 60 * minutes;
 			// ticks * seconds * minutes
 			
 	public CensusInterval() {
